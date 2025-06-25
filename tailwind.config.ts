5e9e48baa51ddc2c33f6e32fbd0ed1b158210a6b
鲨鱼辣椒 <monishine_code@163.com>
2025-06-25 15:15:07 +0800

    feat(deps): 安装heroui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
 import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
+import { heroui } from "@heroui/react";
 
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -20,5 +21,5 @@ export default {
     },
   },
   darkMode: "class",
-  plugins: [],
+  plugins: [heroui()],
 } satisfies Config;
