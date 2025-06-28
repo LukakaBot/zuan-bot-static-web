c1e8cbc0470a33c1ce20289453f7b30bc98bf17b
鲨鱼辣椒 <monishine_code@163.com>
2025-06-28 23:49:50 +0800

    feat: 添加“上一条”功能

--- app/page.tsx
+++ app/page.tsx
@@ -11,20 +11,30 @@ function Home() {
   const [, setLoading] = useLoading();
   const [data, setData] = useState<string>("");
   const [isCurseFatherMode, setCurseFatherMode] = useState(false);
+  const [historyQuotes, setHistoryQuotes] = useState<string[]>([]);
 
-  const getData = useCallback(async (level: string = "min") => {
+  const getData = async (level: string = "min") => {
     try {
       setLoading(true);
       const res = await fetchZuanQuote(level);
       setData(res);
+      setHistoryQuotes([...historyQuotes, res]); // 保存历史数据
     } finally {
       setLoading(false);
     }
-  }, []);
+  };
 
   function copyData(data: string) {
     console.log(data);
     navigator.clipboard.writeText(data);
+  }
+
+  function getPreviousQuote() {
+    if (historyQuotes.length <= 1) return; // 如果只有一条数据，不做任何操作
+    const newHistory = historyQuotes.slice(0, -1); // 删除最后一条数据
+    const previousQuote = newHistory[newHistory.length - 1]; // 获取倒数第二条数据
+    setHistoryQuotes(newHistory);
+    setData(previousQuote);
   }
 
   useEffect(() => {
@@ -34,14 +44,18 @@ function Home() {
   return (
     <div className="flex flex-col gap-5 items-center">
       <div className="w-full">
-        <Textarea className="min-h-[150px]" placeholder="" defaultValue={data} />
+        <Textarea
+          className="min-h-[150px]"
+          placeholder=""
+          defaultValue={data}
+        />
       </div>
-
       <div className="flex flex-col gap-3">
         <div className="flex gap-3">
           <Button onClick={() => getData("max")}>😤火力全开</Button>
           <Button onClick={() => getData()}>🙂口吐莲花</Button>
           <Button onClick={() => copyData(data)}>复制</Button>
+          <Button onClick={getPreviousQuote}>上一条</Button>
         </div>
         <div className="flex gap-3">
           <div
